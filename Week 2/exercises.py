f2a727361806d494993bbb4af4a216c4b7bcb6eb
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-02-12 14:34:51 +1000

    Create exercises.py

---
+++ Week 2/exercises.py
@@ -0,0 +1,36 @@
+EXERCISE 1:
+
+# Define a string variable named ‘name’ and set value to ‘Eric’
+# Define a integer variable named ‘age’ and set value to 25
+# Define a float variable named ‘weight_kg’ and set value to 65.50
+# Print the variables to the console
+
+EXERCISE 2:
+
+# Define a string variable named ‘surname’ and set value to ‘Jones’
+# Make a new variable ‘fullname’ by concatenating name and surname (with a whitespace in between)
+# Make a new variable named ‘weight_pound’ with Eric’s weight in pounds (1kg = 2.20462 pounds)
+# Print the variables fullname and weight_pound to the console (i.e.  The full name followed by “weighs” and then the person’s weight in pounds)
+
+# EXERCISE 3:
+
+# Define an empty list with the name ‘cities’
+# Add the following cities:  ‘Brisbane’, ‘Melbourne’, ‘Sydney’, ‘Perth’
+# Delete ‘Melbourne’
+# Print the list to the console
+# Print the first city to the console
+# Print the last city to the console
+# Check (programmatically!) whether Brisbane is in the list
+
+# EXERCISE 4:
+
+# Define a list with the following words: ‘cow’, ‘sheep’, ‘horse’ and print it to the console
+# Define a list of dictionaries, based on the matrix (below) and print it to the console
+
+# Name  Couse   Grade
+# Mark  Math    A
+# Mark  English A
+# Eric  Math    B
+# Eric  English B
+
+# From that dictionary, print Eric’s and Mark’s math scores to the console
