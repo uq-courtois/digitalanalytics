e309ae3d5b106187d20b6e5865579ca5444b5650
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-02-12 15:47:38 +1000

    Create exercises.py

---
+++ Week 3/exercises.py
@@ -0,0 +1,67 @@
+### EXERCISE 1a:
+
+# Define a list with the following names: Jane, John, Marisa, Kobe, Eliza
+# Print each name on a separate line by looping through the list
+
+### EXERCISE 1b:
+
+# Define a list with the following numbers: 34, 45, 23, 49
+# Divide each number by 2 and print the result on a seperate line
+
+### EXERCISE 2:
+
+# Write a program that filters strings from the following list: 
+# tip: if item == str:
+
+collection = ['Eric',10,34,'Karen',32,'Lisa']
+
+# Print the final list to the console
+
+### EXERCISE 3:
+
+# Write a program that multiplies every number in list x by 5.
+# Print the results in descending order...
+
+x = [4,5,2,4,5,2,4]
+
+### EXERCISE 4:
+
+# Variable lyric is a string. Count the times that the word 'rock' is mentioned and print the result...
+
+lyric = "Everybody rock your body Everybody rock your body right Backstreet's Back alright"
+
+### EXERCISE 5:
+
+# Format the following matrix in the appropriate data structure
+
+# Name  Year of Birth
+# Mark  1976
+# Jane  1999
+# Eric  2002
+
+# Write a program that transforms the years of birth to years of age
+# Add age as a new key in the already given list of dictionaries
+# Print the name and the age of every person if they are over 30 years old
+
+### EXERCISE 6a:
+
+# Print the first five unique urls that contain an .au domain form the list urls
+
+urls = [
+"www.abc.net.au/",
+"www.cnn.com/",
+"10play.com.au/",
+"www.freeview.com.au/",
+"www.freeview.com.au/",
+"www.bbc.co.uk/",
+"www.ariacharts.com.au/",
+"www.9now.com.au/",
+"www.yourtv.com.au/"
+]
+
+### EXERCISE 6b:
+
+# Print the first five unique urls that contain an .au domain form the list urls (i.e., www.freeview.com.au/ is in there twice)
+
+
+
