4a3d05274875fb8d0d20b7e639cec650a11a53a4
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-02-20 18:00:49 +1000

    Create exercises.py

---
+++ Week 7/exercises.py
@@ -0,0 +1,21 @@
+### EXERCISE 2a
+
+# Get the information Spotify returns for the track ‘Dance Monkey’
+# Print the information to the console
+
+# Use the /Search endpoint https://developer.spotify.com/documentation/web-api/reference/search/search/
+# Don’t forget to change type from artist to track in your request + set limit to 1)
+
+### EXERCISE 2b
+
+# Print the artist to the console (i.e., dig and peel in the JSON data until you isolate the artist) 
+
+# Use the /Search endpoint https://developer.spotify.com/documentation/web-api/reference/search/search/
+# Don’t forget to change type from artist to track in your request + set limit to 1)
+
+### EXERCISE 3
+
+# Based on a csv file with 10 artists (artists.csv): what are the genres Spotify provides? How many followers do the artists have?
+# Write results into a new csv file
+
+# Use the /Search endpoint https://developer.spotify.com/documentation/web-api/reference/search/search/
