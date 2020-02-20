eebde0d5a9d22edd77290fa15c571ec4ca857781
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-02-20 12:12:39 +1000

    Create google-exercise.py

---
+++ Week 6/google-exercise.py
@@ -0,0 +1,8 @@
+### What do we need? 
+### Title, description, and url of first page’s organic results for the following three queries:
+
+# "Climate change"
+# "Global warming"
+# "Carbon emmision reduction"
+
+### What is our source? Google.com
