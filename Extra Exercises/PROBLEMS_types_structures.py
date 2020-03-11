e2f7de84a4e609a55bcac65f0ce17bbebeff8624
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-11 23:52:02 +1000

    Create PROBLEMS_types_structures.py

---
+++ Extra Exercises/PROBLEMS_types_structures.py
@@ -0,0 +1,13 @@
+### EXERCISE A
+# Define a list with the following names: Joan, Marc, Jing, Ella
+# Print the third name in the list
+# Change the first name from Joan to June
+# Add Lisa to the list
+# Print the entire list
+
+### EXERCISE B
+# Define a list with the following names: Lisa,  Andrea,  Akira, Sam, Lisa
+# Filter a list with unique names
+# Order alfabethically in the list
+# Print the entire list
+# Print the first two names
