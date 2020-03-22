ee6a1a9b9ba8332f1f7b9f779b5d4d0fca671bb7
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-23 01:25:02 +1000

    Update exercises.py

--- Week 3/exercises.py
+++ Week 3/exercises.py
@@ -1,20 +1,20 @@
-### EXERCISE 1
+### EXERCISE 6
 
-# Define a variable name1 with a value Jane
+# Define a variable name with a value Jane
 # Build a program that prints ‘Found Jane’ when the value of name matches ‘Jane’ 
 # and ‘Jane not found’ when the value does not match ‘Jane’
 
-### EXERCISE 2a
+### EXERCISE 7a
 
 # Define a list with the following names: Jane, John, Marisa, Kobe, Eliza
 # Print each name on a separate line by looping through the list
 
-### EXERCISE 2b:
+### EXERCISE 7b:
 
 # Define a list with the following numbers: 34, 45, 23, 49
 # Divide each number by 2 and print the result on a seperate line
 
-### EXERCISE 3:
+### EXERCISE 8:
 
 # Write a program that filters strings from the following list and
 # stores each of the string values in a new list called newcollection: 
@@ -26,7 +26,7 @@ collection = ['Eric',10,34,'Karen',32,'Lisa']
 
 # Print the final list to the console
 
-### EXERCISE 4:
+### EXERCISE 9:
 
 # Write a program that multiplies every number in list x by 5 
 # and store those new values in a new list that you can use after the for loop is finished.
@@ -34,14 +34,14 @@ collection = ['Eric',10,34,'Karen',32,'Lisa']
 
 x = [4,5,2,4,5,2,4]
 
-### EXERCISE 5:
+### EXERCISE 10:
 
 # Variable lyric is a string. Loop through the individual words.
 # Only print the word ‘rock’ to the console each time it is mentioned.
 
 lyric = "Everybody rock your body Everybody rock your body right Backstreet's Back alright"
 
-### EXERCISE 6:
+### EXERCISE 11:
 
 # Format the following matrix in the appropriate data structure
 
@@ -54,9 +54,16 @@ lyric = "Everybody rock your body Everybody rock your body right Backstreet's Ba
 # Add age as a new key in the already given list of dictionaries
 # Print the name and the age of every person if they are over 30 years old
 
-### EXERCISE 7a:
+### EXERCISE 12a - A harder exercise...
 
 # Print the first five urls that contain an .au domain form the list urls
+
+# TIP 1: use a variable 'count' that starts with a value 0 at the beginning of the script
+# The count variable should update itself with +1 per RELEVANT iteration 
+# to keep count of the number of RELEVANT iterations
+# count += 1 (=> is adding 1 to the original value of the variable)
+
+# TIP 2: if 'break' is used in a for loop, the loops terminates immediately
 
 urls = [
 "www.abc.net.au/",
@@ -70,9 +77,6 @@ urls = [
 "www.yourtv.com.au/"
 ]
 
-### EXERCISE 7b:
+### EXERCISE 12b - Adding just one extra bit...
 
 # Print the first five unique urls that contain an .au domain form the list urls (i.e., www.freeview.com.au/ is in there twice)
-
-
-
