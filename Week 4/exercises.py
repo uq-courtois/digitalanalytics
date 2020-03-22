07438a0c29a5dd693c9a92f202381e6b297122d9
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-23 00:42:29 +1000

    Update exercises.py

--- Week 4/exercises.py
+++ Week 4/exercises.py
@@ -1,9 +1,9 @@
 ### EXERCISE 1:
 
-# Read data from dataset.csv
-# Print the artists in the dataset to the console, line by line
+# Read data from dataset.csv - Download it from < SEE BLACKBOARD LINK >
+# Print the artists and songs in the dataset to the console, line by line
 
-### EXERCISE 2: 
+### EXERCISE 2:
 
 # Name    Couse     Grade
 # Mark    Math      A
@@ -11,18 +11,18 @@
 # Eric    Math      B
 # Eric    English   B
 
-# Enter data from table
+# Enter data from table into your script (i.e., hard code it)
 # Write it into a new csv file ‘grades.csv’
 
-### EXERCISE 3:
+### EXERCISE 3
 
-# Read the data from searchresults.csv (on the GitHub page in the Week 5 folder)
+# Read the data from searchresults.csv (< See Blackboard item for URL >)
 # Print the unique domain names extracted from all results (url variable in the data file)
 # Count the number of times ‘hln.be’ is in the results
-# Make a new csv file with only the top 3 ranked results (call it searchresults_top3.csv)
+# Make a new csv file with only the top 3 ranked results (name it searchresults_top3.csv)
 
 ### EXERCISE 4:
 
-# Read the data from instagramdata.csv (on the GitHub page in the Week 5 folder)
+# Read the data from instagramdata.csv (< SEE LINK ON BLACKBOARD >)
 # Convert the followers and following variables into proper integers (K = 1,000, M = 1,000,000)
 # Make a new csv file with the new data (name it instagram_clean.csv)
