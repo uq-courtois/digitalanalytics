bf2c69083f2df79d5cd09efdbe8194c3b7b05521
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-27 00:24:31 +1000

    Create cage.py

---
+++ Week 5/cage.py
@@ -0,0 +1,43 @@
+from urllib.request import Request, urlopen
+import ssl
+from bs4 import BeautifulSoup
+
+# The URL we want to scrape
+url = 'https://www.imdb.com/name/nm0000115/'
+
+#################################################
+#################################################
+### COPY/PASTE THIS BLOCK AS IS
+
+# Open URL (i.e., make request) + disguise agent
+headers={'User-Agent': 'Mozilla/5.0 (Macinstosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+req = Request(url, headers=headers)
+context = ssl._create_unverified_context()
+
+# To fetch html and store in variable 'html'
+uClient= urlopen(req, context=context)
+html = uClient.read() # html is stored in variable html
+uClient.close()
+
+#################################################
+#################################################
+
+### Interpret the page source as html
+soup = BeautifulSoup(html, 'html.parser')
+
+cagemovies = []
+
+for item in soup.find_all('div', class_ = "filmo-row odd"):
+	title = item.find('a').getText().strip()
+	year = item.find('span', class_ = 'year_column').getText().strip()
+	print(title+' ('+str(year)+')')
+	cagemovies.append(title)
+
+for item in soup.find_all('div', class_ = "filmo-row even"):
+	title = item.find('a').getText().strip()
+	year = item.find('span', class_ = 'year_column').getText().strip()
+	print(title+' ('+str(year)+')')
+	cagemovies.append(title)
+
+print()
+print('Found',len(cagemovies),'Nicolas Cage productions')
