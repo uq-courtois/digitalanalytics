1cb9d052b9b0a55072a0f3b3c388d9ff8258da09
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-03-30 02:36:42 +1000

    Update geocoding.py

--- Week 6/geocoding.py
+++ Week 6/geocoding.py
@@ -7,7 +7,7 @@ baseurl = "https://maps.googleapis.com/maps/api/geocode/json?" # Endpoint URL
 query = "address=" + "University of Queensland" # Build query for address argument
 query = query.replace(' ','+') # Replace spaces in query with +
 apikey = "&key=AIzaSyDC60i9o-E4sOVCsYCZCYGB3DIlhAenZy0" # API key - you will need to get your own to try this example
-# I de-actived the API key in this example for safety reasons
+# I deactivated the API key in this example for safety reasons
 
 compiledurl = baseurl + query + apikey # Combining all the url components into a single url
 
