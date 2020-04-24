536a80a96f159693d82c6c94c660d0372ddab2de
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2020-04-24 20:08:13 +1000

    Update abcnews.py

--- Week 5/abcnews.py
+++ Week 5/abcnews.py
@@ -1,3 +1,16 @@
+# IMPORTANT!!! THIS VERSION WAS EDITED AFTER THE VIDEO WAS MADE DUE TO CHANGES TO THE ABC.NET.AU WEBSITE. THIS SOLUTION IS VALID, THE VIDEO ONLY SHOWS THE APPROACH
+
+
+#Build a scraper that tracks ABC's latest news articles on #https://www.abc.net.au/news/justin/.
+
+#You should scrape the news article title and the hyperlink that links to it. Start from the top, and stop scraping just before the 'load more stories' button. Print the results to the console. An add-on: automatically calculate the percentage of articles that are on the COVID-19/Corona virus at the end.
+
+#TIP 1: First narrow down the value of your soup variable (all the html) to that of the div that holds all of the news updates we are interested in...
+
+#TIP 2: If you want to check whether a certain substring is in a string value, but you don't want to be hindered by upper/lower case restrictions, it might be helpful to convert your string variable into lower case by using the .lower() method - e.g., for the string variable varx >>> varx.lower()
+
+#TIP 3: You can use len(listx) to get the length of a list named listx
+  
 #Build a scraper that tracks ABC's latest news articles on #https://www.abc.net.au/news/justin/.
 
 #You should scrape the news article title and the hyperlink that links to it. Start from the top, and stop scraping just before the 'load more stories' button. Print the results to the console. An add-on: automatically calculate the percentage of articles that are on the COVID-19/Corona virus at the end.
@@ -34,12 +47,12 @@ uClient.close()
 
 ### Interpret the page source as html
 soup = BeautifulSoup(html, 'html.parser')
-maindiv = soup.find('div',class_='sidebar__main--6TJ0W')
+maindiv = soup.find('div',class_='_6TJ0W')
 
 allarticles = []
 covidarticles = []
 
-for item in maindiv.find_all('div',class_='card-layout__content--38hRP'):
+for item in maindiv.find_all('div',class_='_38hRP'):
 	title = item.find('a').getText()
 	link = 'http://abc.net.au'+item.find('a')['href']
 
